eaf773380f3664c3a65bfa96fe8c421dfc787140
nanaVybes <xquame@yahoo.com>
2023-03-19 03:45:15 +0000

    voltssh_vps_manager

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# voltssh_manager
-voltssh_manager
+# VOLT_SSH_MANAGER
+
+
+```
+apt install wget -y; bash <(wget -qO- raw.githubusercontent.com/prjkt-nv404/voltssh_manager/main/ssh-plus)
+```
---
+++ startbanner.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+  echo 'clear' >>.bashrc
+    echo 'echo ""' >>.bashrc
+    echo 'echo -e "\t\033[91m " ' >>.bashrc
+    echo 'echo -e "\t\033[91m                       ████   █████                    █████ " ' >>.bashrc    
+    echo 'echo -e "\t\033[91m                      ░░███  ░░███                    ░░███       " ' >>.bashrc
+    echo 'echo -e "\t\033[91m  █████ █████  ██████  ░███  ███████    █████   █████  ░███████   " ' >>.bashrc
+    echo 'echo -e "\t\033[91m ░░███ ░░███  ███░░███ ░███ ░░░███░    ███░░   ███░░   ░███░░███  " ' >>.bashrc
+    echo 'echo -e "\t\033[91m  ░███  ░███ ░███ ░███ ░███   ░███    ░░█████ ░░█████  ░███ ░███  " ' >>.bashrc
+    echo 'echo -e "\t\033[91m  ░░███ ███  ░███ ░███ ░███   ░███ ███ ░░░░███ ░░░░███ ░███ ░███  " ' >>.bashrc
+    echo 'echo -e "\t\033[91m   ░░█████   ░░██████  █████  ░░█████  ██████  ██████  ████ █████ " ' >>.bashrc
+    echo 'echo -e "\t\033[91m     ░░░░░     ░░░░░░  ░░░░░    ░░░░░  ░░░░░░  ░░░░░░  ░░░░ ░░░░░ " ' >>.bashrc
+    echo 'echo "" ' >>.bashrc
+    echo 'echo "" ' >>.bashrc
+    echo 'echo -e "\t\033[92mTelegram   : @VoltsshBot, @voltssh " ' >>.bashrc
+    echo 'echo -e "\t\e[1;33mPowered by : AIB Tech Pvt."' >>.bashrc
+    echo 'echo ""' >>.bashrc
+    echo 'DATE=$(date +"%d-%m-%y")' >>.bashrc
+    echo 'TIME=$(date +"%T")' >>.bashrc
+    echo 'echo -e "\t\e[1;33mSERVER NAME : $HOSTNAME"' >>.bashrc
+    echo 'echo -e "\t\e[1;33mSERVER CONNECTED TO : $(uptime -p)"' >>.bashrc
+    echo 'echo -e "\t\e[1;33mDATE : $DATE"' >>.bashrc
+    echo 'echo -e "\t\e[1;33mTIME : $TIME"' >>.bashrc
+    echo 'echo "" ' >>.bashrc
+    echo 'echo -e "\t\033[97mTO DISPLAY BASH PANEL TYPE: sudo menu Or menu "' >>.bashrc
+    echo 'echo -e ""' >>.bashrc
